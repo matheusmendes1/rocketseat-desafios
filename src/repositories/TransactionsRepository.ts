b793ebfb3622e4d9ff28f15caacf040d9a74d842
Matheus Santos <matheusmendes1@hotmail.com>
2020-04-16 21:24:51 -0300

    Desafio - Fundamentos NodeJS v1

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const transactionsTypeIncome = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((accumulator, transaction) => {
+        return accumulator + transaction.value;
+      }, 0);
+
+    const transactionsTypeOutcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((accumulator, transaction) => {
+        return accumulator + transaction.value;
+      }, 0);
+
+    const totalTransactions = transactionsTypeIncome - transactionsTypeOutcome;
+
+    return {
+      income: transactionsTypeIncome,
+      outcome: transactionsTypeOutcome,
+      total: totalTransactions,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
